6157d136ab174e3aa6c0a19410fde93bb89a2722
Shaun Ryan <Shaun_Chiburi@hotmail.com>
2021-03-06 20:25:20 +0000

    chore: ci cd cluster

---
+++ databricks/init_scripts/ml_small_8.0.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+export ENVIRONMENT=DEV
+export AZUREADID=a69c8df4-e648-4b0a-beb9-b3716a01f60e
+export LOGAPPALERTEMAIL=shaun_chibru@hotmail.com
+export AUTOMATIONSCOPE=azure-key-vault-scope
+export STORAGE=AzureDataLakeGen2
+export STORAGEACCOUNT=abfss://datalake@datalakegeneva.dfs.core.windows.net/
+export RESOURCEGROUP=DataPlatform
+export SUBSCRIPTIONID=e95203c2-64a0-43f9-bfc5-a4fdc588571a
+export LOGAPPALERTENDPOINT=LOGAPP-ALERT-ENDPOINT
+export DATAPLATFORMAPPID=DATALAKE-SPN-APPID
+export DATAPLATFORMSECRET=DATALAKE-SPN-CREDENTIAL
+export LOGGINGLEVELOVERRIDE=ERROR
+export PIPELINEPROJECTSDIR=/dbfs/FileStore/pipelineProjects/
+
+pip install /dbfs/FileStore/deployment/wheels/mycelium-0.1.0-py3-none-any.whl
--- deployment/deployment.py
+++ deployment/deployment.py
@@ -1,10 +1,10 @@
 import os, fnmatch, sys
 
-
 os.environ["DATABRICKS_API_HOST"] = sys.argv[1]
 os.environ["DBUTILSTOKEN"] = sys.argv[2]
 
-from spores import Workspace, Jobs, Dbfs
+from spores.Dbfs import find_file, dbfs_upload
+from spores.Clusters import clusters_create
 from uuid import uuid4
 from pprint import pprint
 
@@ -12,11 +12,20 @@ build_dir = sys.argv[3]
 deploy_dir = sys.argv[4]
 filename = sys.argv[5]
 
-wheels = Dbfs.find_file('*.whl', build_dir)
+# deploy wheel
+wheel_dir = f"{deploy_dir}/dist"
+wheels = find_file('*.whl', wheel_dir)
 
 for whl in wheels:
 
     whl_filename = os.path.basename(whl)
 
     print(f"{whl} => {deploy_dir}/{whl_filename}")
-    Dbfs.dbfs_upload(whl, f"{deploy_dir}/{whl_filename}", True)
+    dbfs_upload(whl, f"{deploy_dir}/{whl_filename}", True)
+
+# deploy clusters
+cluster_dir = f"{deploy_dir}/databricks/clusters"
+init_scripts_dir = f"{deploy_dir}/databricks/init_scripts"
+
+clusters_create(cluster_dir, delete_if_exists=True, init_script_path=init_scripts_dir)
+
