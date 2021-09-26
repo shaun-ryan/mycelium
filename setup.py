68b737d1387055b48fb21e2285d3f7cccac76ec7
Shaun Ryan <Shaun_Chiburi@hotmail.com>
2021-09-26 10:17:51 +0100

    chore: upgrade to python 3.8

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     packages=["mycelium"],
     install_requires=[
